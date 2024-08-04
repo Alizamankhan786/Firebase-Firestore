dc887436b2fa831ca3664f57682f455e902bb9cc
Alizamankhan786 <az030366@gmail.com>
2024-08-04 10:49:51 -0700

    half done

--- app.js
+++ app.js
@@ -1,40 +1,150 @@
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"; 
-import { db } from "./config.js";
+import {
+    collection,
+    addDoc,
+    getDocs,
+    doc,
+    deleteDoc,
+    updateDoc,
+    Timestamp,
+    query,
+    where,
+    orderBy,
+  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+  import { db } from "./config.js";
+
+
+
 
 
 const form = document.querySelector(`#form`);
 const input = document.querySelector(`#firestore`);
 const ul = document.querySelector(`#list`);
+const select = document.querySelector(`#select`);
+const cities = document.querySelectorAll(`.k1`);
+const reset = document.querySelector(".reset"); 
 
-const array = [];
 
-async function getData(){
-    const querySnapshot = await getDocs(collection(db , `posts`));
-    querySnapshot.forEach((doc) => {
-    console.log(doc.data());
-    array.push(doc.data());
+
+let array = [];
+
+
+
+
+
+// CITIES DEFINE 
+
+cities.forEach((btn) => {
+    btn.addEventListener("click", async (event) => {
+      array = [];
+      console.log(event.target.innerHTML);
+      const todosRef = collection(db, "posts");
+      const q = query(
+        todosRef,
+        where("city", "==", event.target.innerHTML),
+        orderBy("time", "desc")
+      );
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        array.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(array);
+      renderTodo();
     });
-    console.log(array);
-    renderTodo();
-    
-}
+  });
 
-getData();
 
+
+
+
+
+
+// RESET BUTTON
+
+reset.addEventListener("CLICK" , getData);
+
+    async function getData(){
+        array = [];
+        const q = query(collection(db , "posts") , orderBy("time" , "desc"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            array.push({ ...doc.data() , id: doc.id });
+        });
+        console.log(array);
+        renderTodo();       
+    }
+
+
+
+
+
+
+// RENDER DATA ON SCREEN
 
 function renderTodo(){
-    ul.innerHTML = ``;
+
+    ul.innerHTML = "";
     if(array.length === 0){
-        ul.innerHTML = "No data found";
+        ul.innerHTML = "";
         return;
     }
 
     array.map((item) => {
         ul.innerHTML += `
         <li>${item.posts}</li>
+        <button class="deleteBtn btn btn-primary">Delete</button>
+        <button class="editBtn btn btn-primary">Edit</button>
+        <p>${item.time ? item.time.toDate() : ""}</p>
+        <hr>
         `;
     });
-}
+};
+
+
+
+
+
+
+// Delete and Edit Button
+
+const deletebtn = document.querySelectorAll(`#delete`);
+const editbtn = document.querySelectorAll(`#edit`);
+
+// Delete Button
+
+
+deletebtn.forEach((btn , index) => {
+    btn.addEventListener("click" , async () => {
+    console.log(array[index]);
+    await deleteDoc(doc(db , "posts" , array[index].id));
+    console.log("Data Deleted");
+    array.splice(index , 1);
+    renderTodo();
+    });
+});
+
+
+
+
+
+// Edit Button
+
+editbtn.forEach((btn , index) => {
+    btn.addEventListener("click" , async () => {
+        const updatedNewValue = prompt("Enter New Value");
+        const todoUpdate = doc(db , "posts" , array[index].id);
+        await updateDoc(todoUpdate , {
+        posts:updatedNewValue,
+        });
+        console.log("Data Updated");
+        array[index].posts = updatedNewValue;
+        renderTodo();
+    });
+});
+
+
+
+
+
 
 
 form.addEventListener(`submit` , async (event) => {
@@ -46,7 +156,6 @@ form.addEventListener(`submit` , async (event) => {
     try {
         const docRef = await addDoc(collection(db, "posts"), {
         input: input.value,
-        uid: auth.currentUser.uid
         });
         input.value = ``;
         console.log("Document written with ID: ", docRef.id);
